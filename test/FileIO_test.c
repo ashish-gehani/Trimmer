095f74a213ae74608360aa24647ce861094c2269
hsharif3 <hsharif3@illinois.edu>
2016-05-22 03:17:22 -0500

    Adding clean test case for file rad

--- test/FileIO_test.c
+++ test/FileIO_test.c
@@ -10,26 +10,29 @@
  
 
 int main(int argc, char* argv[]) {
- 
+  
+    int n;
     int input_fd, output_fd;    /* Input and output file descriptors */
     ssize_t ret_in, ret_out;    /* Number of bytes returned by read() and write() */
     char buffer[BUF_SIZE];      /* Character buffer */
- 
-    
+  
+    // printf("enter value for n\n");
+    //scanf("%d", &n);
+
     /* Create input file descriptor */
     input_fd = open ("staticFile", O_RDONLY);
     if (input_fd == -1) {
-            perror ("open");
-            return 2;
+      perror ("open");
+      return 2;
     }
  
-    ret_in = read (input_fd, &buffer, 10);
-    
-    printf("buffer value %s \n", buffer);	
-    printf("bytes returned %d \n", ret_in);
+   
+    while ((read (input_fd, &buffer, 10) == 0))
+      printf("buffer value %s \n", buffer);	
 
     /* Close file descriptors */
     close (input_fd);
  
     return (EXIT_SUCCESS);
-}
+}    
+
