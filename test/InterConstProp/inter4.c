4a92ac5352a0f5b794e5bbfbd1fa929b5051787f
hsharif3 <hsharif3@illinois.edu>
2017-07-11 20:23:30 -0500

    Adding test for Multiple Function Arguments

---
+++ test/InterConstProp/inter4.c
@@ -0,0 +1,28 @@
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+
+void func(char * buffer, char * buffer2){
+  
+  if(strcmp(buffer, buffer2) == 0)
+    printf("Both strings are equal\n"); 
+}
+ 
+
+int main(){
+
+  char buffer[100];
+  memcpy(buffer, "value=key", 100);
+  char buffer2[100];
+  memcpy(buffer2, "value=key", 100);
+
+  if(buffer[90] == '\n') {
+    printf("random check for testing node merging \n");
+  }
+
+  func(buffer, buffer2);
+
+  return 0;
+}
