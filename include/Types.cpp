e4375a766d31272ff0a91505f472e5e73fff7a19
muhammad <bakar95@gmail.com>
2017-07-13 16:57:25 +0500

    a few structural changes

--- include/Types.cpp
+++ include/Types.cpp
@@ -1,37 +1,3 @@
-#include "llvm/Pass.h"
-#include "llvm/IR/Function.h"
-#include "llvm/Transforms/Utils/BasicBlockUtils.h"
-#include "llvm/Analysis/InstructionSimplify.h"
-#include "llvm/IR/Module.h"
-#include "llvm/Analysis/CallGraph.h"
-#include "llvm/IR/Instruction.h"	
-#include "llvm/IR/Instructions.h"
-#include "llvm/IR/GlobalValue.h"
-#include "llvm/Support/raw_ostream.h"
-#include "llvm/IR/InstIterator.h"
-#include "llvm/IR/Constants.h"
-#include "llvm/IR/Operator.h"
-#include "llvm/Analysis/MemoryDependenceAnalysis.h"
-#include "llvm/IR/IntrinsicInst.h"
-#include "llvm/ADT/SmallVector.h"
-#include "llvm/Transforms/Utils/SimplifyLibCalls.h"
-#include "llvm/Analysis/TargetLibraryInfo.h"
-#include "llvm/Transforms/Utils/BasicBlockUtils.h"
-#include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/Dominators.h"
-#include "llvm/IR/ValueMap.h"
-#include "llvm/Transforms/Utils/Cloning.h"
-#include <sys/stat.h>
-#include <map>
-#include <set>
-#include <iostream>
-#include <vector>
-#include <map>
-#include <fstream>
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <sstream>
 #include "Types.h"
 
 using namespace llvm;
