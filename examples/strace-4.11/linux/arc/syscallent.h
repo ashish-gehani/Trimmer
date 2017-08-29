57e55db0058bc3b4fd380d30eac38a449c5d5a08
muhammad <17100056@lums.edu.pk>
2017-08-29 22:16:36 +0500

    added strace

---
+++ examples/strace-4.11/linux/arc/syscallent.h
@@ -0,0 +1,6 @@
+#define ARCH_mmap mmap_pgoff
+#include "32/syscallent.h"
+[244] = { 3,	0,	SEN(printargs),	"arc_cacheflush"},
+[245] = { 1,	0,	SEN(printargs),	"arc_settls"	},
+[246] = { 0,	0,	SEN(printargs),	"arc_gettls"	},
+[247 ... 259] = { },
---
+++ examples/strace-4.11/linux/nios2/syscallent.h
@@ -0,0 +1,4 @@
+#define ARCH_mmap mmap_pgoff
+#include "32/syscallent.h"
+[244] = {4,    0,	SEN(cacheflush), "cacheflush"},
+[245 ... 259] = { },
