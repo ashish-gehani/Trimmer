649da6c4802fcf61e048efb9d6ccda14775059c6
aatiranum <aatira.anum@gmail.com>
2018-06-28 01:03:58 -0700

    Add files via upload

--- test/InterConstProp/script.sh
+++ test/InterConstProp/script.sh
@@ -1,12 +1,11 @@
 cd ${TRIMMER_HOME}/test/InterConstProp
-
-${LLVM_CC_NAME} -O0 -emit-llvm -o workdir/${1}t1.bc -c ${1}.c
-${LLVM_OPT_NAME} -mem2reg -loops -lcssa -loop-rotate -loop-simplify -mem2reg -simplifycfg workdir/${1}t1.bc -o workdir/${1}t2.bc
-${LLVM_OPT_NAME} -load ${TRIMMER_HOME}/build/InterConstProp.so -isAnnotated=false -inter-constprop workdir/${1}t2.bc -o workdir/${1}t3.bc
-${LLVM_OPT_NAME} -load ${TRIMMER_HOME}/build/Remove.so -remove workdir/${1}t3.bc -o workdir/${1}t3.bc 
-${LLVM_OPT_NAME} -O3 workdir/${1}t3.bc -o workdir/${2}.bc
-${LLVM_DIS_NAME}  workdir/${1}t1.bc
-${LLVM_DIS_NAME}  workdir/${1}t2.bc
-${LLVM_DIS_NAME}  workdir/${1}t3.bc
-${LLVM_DIS_NAME}  workdir/${2}.bc
-${LLVM_OPT_NAME} -load ${TRIMMER_HOME}/build/testPass.so -testing workdir/${2}.bc -o workdir/${2}.bc
+mkdir -p $3
+${LLVM_CC_NAME} -O0 -emit-llvm -o $3/${1}t1.bc -c ${1}.c
+${LLVM_OPT_NAME} -mem2reg -loops -lcssa -loop-rotate -loop-simplify -mem2reg -simplifycfg $3/${1}t1.bc -o $3/${1}t2.bc
+${LLVM_OPT_NAME} -load ${TRIMMER_HOME}/build/InterConstProp.so -isAnnotated=false -inter-constprop $3/${1}t2.bc -o $3/${1}t3.bc
+${LLVM_OPT_NAME} -O3 $3/${1}t3.bc -o $3/${2}.bc
+${LLVM_DIS_NAME}  $3/${1}t1.bc
+${LLVM_DIS_NAME}  $3/${1}t2.bc
+${LLVM_DIS_NAME}  $3/${1}t3.bc
+${LLVM_DIS_NAME}  $3/${2}.bc
+${LLVM_OPT_NAME} -load ${TRIMMER_HOME}/build/testPass.so -testing $3/${2}.bc -o $3/${2}.bc
