12bebdd4a6ded00e0c217e6918c4469756887973
muhammad <17100056@lums.edu.pk>
2018-06-29 00:42:25 +0500

    minor change to issue 6

--- test/InterConstProp/script.sh
+++ test/InterConstProp/script.sh
@@ -1,5 +1,4 @@
 cd ${TRIMMER_HOME}/test/InterConstProp
-mkdir -p $3
 ${LLVM_CC_NAME} -O0 -emit-llvm -o $3/${1}t1.bc -c ${1}.c
 ${LLVM_OPT_NAME} -mem2reg -loops -lcssa -loop-rotate -loop-simplify -mem2reg -simplifycfg $3/${1}t1.bc -o $3/${1}t2.bc
 ${LLVM_OPT_NAME} -load ${TRIMMER_HOME}/build/InterConstProp.so -isAnnotated=false -inter-constprop $3/${1}t2.bc -o $3/${1}t3.bc
--- test/InterConstProp/test.py
+++ test/InterConstProp/test.py
@@ -1,13 +1,16 @@
 import subprocess
 import sys, os
 
-work_dir = str(sys.argv[1])
-lo = int(sys.argv[2])
-hi = int(sys.argv[3])
-
+lo = int(sys.argv[1])
+hi = int(sys.argv[2])
+workdir = "workdir"
+if len(sys.argv) > 3:
+	workdir = str(sys.argv[3])
+if not os.path.exists(workdir):
+    os.makedirs(workdir)
 for i in xrange(lo, hi + 1):
 	if not os.path.isfile('inter' + str(i) + '.c'):
 		continue	
 	print 'running test ' + str(i)
-	Cmd = './script.sh inter' + str(i) + ' out' + str(i) + ' ' + work_dir
+	Cmd = './script.sh inter' + str(i) + ' out' + str(i) + ' ' + workdir
 	subprocess.call(Cmd, shell = True)	
