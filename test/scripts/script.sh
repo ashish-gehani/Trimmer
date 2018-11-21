938fc5ddac062456c995b9d5b78999f41ac7a679
usama54321 <usama54321@gmail.com>
2018-11-21 14:13:41 +0500

    Fixed so file name in test script

--- test/scripts/script.sh
+++ test/scripts/script.sh
@@ -1,7 +1,7 @@
 cd ${TRIMMER_HOME}/test/src
 ${LLVM_CC_NAME} -O0 -emit-llvm -o $3/${1}t1.bc -c ${1}.c
-${LLVM_OPT_NAME} -mem2reg -loops -lcssa -loop-rotate -loop-simplify -mem2reg -simplifycfg $3/${1}t1.bc -o $3/${1}t2.bc
-${LLVM_OPT_NAME} -load ${TRIMMER_HOME}/build/InterConstProp.so -isAnnotated=false -inter-constprop $3/${1}t2.bc -o $3/${1}t3.bc
+${LLVM_OPT_NAME} -mem2reg -loops -lcssa -loop-rotate -loop-simplify -mem2reg -indvars -simplifycfg -O0 $3/${1}t1.bc -o $3/${1}t2.bc
+${LLVM_OPT_NAME} -load ${TRIMMER_HOME}/build/ConstantFolding.so -isAnnotated=false -inter-constprop $3/${1}t2.bc -o $3/${1}t3.bc
 ${LLVM_OPT_NAME} -O3 $3/${1}t3.bc -o $3/${2}.bc
 ${LLVM_DIS_NAME}  $3/${1}t1.bc
 ${LLVM_DIS_NAME}  $3/${1}t2.bc
