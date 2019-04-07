6f29e74f82dcc37b49ed7dbf7843ca6859fdc0f3
Rafae Noor <abdurrafae98@live.com>
2019-04-07 20:51:49 +0500

    fix getopt_long

--- src/GetOptLocal.cpp
+++ src/GetOptLocal.cpp
@@ -1,5 +1,23 @@
 #include "VecUtils.h"
 #include "GetOptLocal.h"
+#include "llvm/Support/raw_ostream.h"
+#include <cstring>
+
+
+int contains(char* str, char c){
+  if(!str)
+    return -1;
+
+  unsigned len = strlen(str);
+
+  for(unsigned i=0;i<len;i++){
+    if(str[i] == c){
+      return i;
+    }
+  }
+
+  return -1;
+}
 
 string err_local;
 int getopt_local(int argc, char ** argv, char * opts) {
@@ -36,29 +54,51 @@ int getopt_local(int argc, char ** argv, char * opts) {
 }
 
 int getopt_long_local(int argc, char ** argv, char * opts, struct option * long_opts, int * long_index) {
-  if(optind == argc) return -1;
+  errs()<<"optind: "<<optind<<", argc: "<<argc<<"\n";
+  if(optind >= argc) return -1;
   int i;
   int opt = -1;
   for(i = optind; i < argc; i++) {
+    errs()<<"argv["<<i<<"]: "<<argv[i]<<"\n";
     if(argv[i][0] != '-') continue;
     opt = argv[i][1];
     break;
   }
   if(opt == -1)
     return -1;
+  errs()<<"i: "<<i<<", opt: "<<opt<<"\n";
 
   if(optind >= argc) return -1;
   optind = i;
-  if(argv[i][1] != '-') return getopt_local(argc, argv, opts);
-  for(i = 0; long_opts[i].name && strcmp(long_opts[i].name, &argv[optind][2]); i++) {}
+  if(argv[i][1] != '-') return getopt_local(argc, argv, opts); // -c instead of --c
+  bool found = false;
+  int hasEqual = contains(&argv[optind][2],'=');
+
+  for(i = 0; long_opts[i].name && !found ; i++) {
+    if(hasEqual != -1 && strlen(long_opts[i].name) >= hasEqual){
+      found = strncmp(long_opts[i].name,&argv[optind][2],hasEqual) == 0;
+      errs()<<"i: "<<i<<", comparing "<<long_opts[i].name<<" with "<<&argv[optind][2]<<", strncmp on n "<<hasEqual<<": "<< strncmp(long_opts[i].name, &argv[optind][2],hasEqual)<<"\n";
+    } else {
+      found = strcmp(long_opts[i].name, &argv[optind][2]) == 0;
+      errs()<<"i: "<<i<<", comparing "<<long_opts[i].name<<" with "<<&argv[optind][2]<<", strcmp: "<< strcmp(long_opts[i].name, &argv[optind][2])<<"\n";
+    }
+    if(found)
+      break;
+  }
   if(!long_opts[i].name) { 
     err_local = "option not found in long_opts\n";
     optind++;
     return '?';
   }  
   if(long_opts[i].has_arg) {
-    if(optind < argc - 1 && argv[optind + 1][0] != '-') {
+    //errs()<<long_opts[i].name<<" has args\n";
+    if(hasEqual != -1 && found){
+      optarg = &argv[optind][hasEqual+2+1];//2 for -- and 1 for = 
+      errs()<<"optarg: "<<optarg<<"\n";
+      optind++;
+    } else if(optind < argc - 1 && argv[optind + 1][0] != '-') {
       optarg = argv[optind + 1];
+      errs()<<"optarg: "<<optarg<<"\n";
       optind++;
     } else {  
       err_local = "optarg not specified\n";
@@ -72,5 +112,6 @@ int getopt_long_local(int argc, char ** argv, char * opts, struct option * long_
     *long_opts[i].flag = long_opts[i].val;
     return 0;
   } 
+  errs()<<"Returning: "<<long_opts[i].val<<"\n";
   return long_opts[i].val;
 }
