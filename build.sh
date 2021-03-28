16ac5c87ccb3030b8611209a5c9d7365b12c6710
mudbri <mubashiranwar@live.com>
2021-03-28 23:49:55 +0000

    Trimmer build changes

--- build.sh
+++ build.sh
@@ -57,29 +57,29 @@ sudo ldconfig -v
 #  Download LLVM source files
 
 echo "=== Downloading LLVM & Clang Source Files ==="
-mkdir llvm7 
-cd llvm7
+# mkdir llvm7 
+# cd llvm7
 
-wget https://releases.llvm.org/7.0.0/llvm-7.0.0.src.tar.xz 
-wget https://releases.llvm.org/7.0.0/cfe-7.0.0.src.tar.xz 
-tar xf llvm-7.0.0.src.tar.xz
-tar xf cfe-7.0.0.src.tar.xz
+# wget https://releases.llvm.org/7.0.0/llvm-7.0.0.src.tar.xz 
+# wget https://releases.llvm.org/7.0.0/cfe-7.0.0.src.tar.xz 
+# tar xf llvm-7.0.0.src.tar.xz
+# tar xf cfe-7.0.0.src.tar.xz
 
-mv cfe-7.0.0.src/ llvm-7.0.0.src/tools/clang
+# mv cfe-7.0.0.src/ llvm-7.0.0.src/tools/clang
 
 
 
 
 #  Build LLVM-7 from source
 
-echo "=== Starting the build process ==="
+# echo "=== Starting the build process ==="
 
-mkdir llvm-7.0.0.obj && cd llvm-7.0.0.obj
-cmake -DCMAKE_BUILD_TYPE=MinSizeRel ../llvm-7.0.0.src
-make -j2
+# mkdir llvm-7.0.0.obj && cd llvm-7.0.0.obj
+# cmake -DCMAKE_BUILD_TYPE=MinSizeRel ../llvm-7.0.0.src
+# make -j2
 
-sudo make install
-cd ../
+# sudo make install
+# cd ../
 
 
 
@@ -90,7 +90,7 @@ echo "=== Exporting required environment variables ==="
 
 export LLVM_SRC=$(pwd)/llvm-7.0.0.src
 export LLVM_OBJ=$(pwd)/llvm-7.0.0.obj
-export LLVM_DIR=$(pwd)/llvm-7.0.0.obj
+export LLVM_DIR=/usr/local/llvm-10.0
 export PATH=$LLVM_DIR/bin:$PATH
 
 
@@ -102,7 +102,7 @@ git clone https://github.com/SVF-tools/SVF.git SVF
 
 #  Specific commit used for TRIMMER development
 cd SVF
-git checkout 0b75f3e0c10db04d65b6eafd91da5da9be71ecaa
+# git checkout 0b75f3e0c10db04d65b6eafd91da5da9be71ecaa
 
 # Add -fPIC to CMakeLists.txt in SVF
 sed -i '/set(CMAKE_CXX_FLAGS "-std=gnu++11 -O3 -fno-rtti")/c\\t\tset(CMAKE_CXX_FLAGS "-std=gnu++11 -O3 -fno-rtti -fPIC")' CMakeLists.txt 
