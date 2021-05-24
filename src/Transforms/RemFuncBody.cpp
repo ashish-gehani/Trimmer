4bdaffe8cf316d18be146170d8853766b248f9eb
aatiranum <aatira.anum@gmail.com>
2021-05-24 06:34:31 -0700

    Update RemFuncBody.cpp

--- src/Transforms/RemFuncBody.cpp
+++ src/Transforms/RemFuncBody.cpp
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//This pass removes the function whose name is given as an argument to the pass.
 
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Module.h"
