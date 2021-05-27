e6c5892bf4357d12073fe89d063a76a48111a826
aatiranum <aatira.anum@gmail.com>
2021-05-27 03:50:56 -0700

    Add comment at top of file

--- src/AnnotateTest.cpp
+++ src/AnnotateTest.cpp
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//This pass is used to test annotation pass.
+
 #include "AnnotateTest.h"
 #include "llvm/IR/GlobalValue.h"
 #include "llvm/Support/raw_ostream.h"
