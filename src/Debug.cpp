7b008d84956247c0cbc0b556a1accb95b61abe3d
aatiranum <aatira.anum@gmail.com>
2021-05-27 04:07:55 -0700

    Add comment at top of file

--- src/Debug.cpp
+++ src/Debug.cpp
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//This file contains methods regarding debugging such as initailzaing debugging Level, printing memory, instructions and basic blocks.
+
 #include "llvm/IR/Instruction.h"	
 
 #include <string>
