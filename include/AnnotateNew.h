d47e5cfcc78d61d1665156a305563a74e14ab0f6
aatiranum <aatira.anum@gmail.com>
2021-05-28 03:38:08 -0700

    Add comment at top of file

--- include/AnnotateNew.h
+++ include/AnnotateNew.h
@@ -4,6 +4,9 @@
  * license that can be found in the LICENSE file.
  */
 
+/* This is the main header file for the Annotation Pass. It defines a class AnnotateNew, which is inherited from LLVM Module Pass. All the methods 
+of the class are defined in src/AnnotateNew.cpp.*/
+
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Module.h"
