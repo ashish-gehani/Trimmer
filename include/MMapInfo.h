cae4e69ee41646a66374245193c7b298685d1260
aatiranum <aatira.anum@gmail.com>
2021-05-28 04:12:47 -0700

    Add comment at top of file

--- include/MMapInfo.h
+++ include/MMapInfo.h
@@ -4,6 +4,8 @@
  * license that can be found in the LICENSE file.
  */
 
+// This file consists of a structure MMapInfo, which is used to bridge the gap between mmap and munmap instructions. 
+
 #ifndef M_MAP_INFO_H
 #define M_MAP_INFO_H
 # include <stdio.h>
