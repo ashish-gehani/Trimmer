6889c0918888d5cae3d0a0219ee8a86826b72125
aatiranum <aatira.anum@gmail.com>
2021-05-28 04:13:09 -0700

    Add comment at top of file

--- include/MMapInfo.h
+++ include/MMapInfo.h
@@ -10,9 +10,6 @@
 #define M_MAP_INFO_H
 # include <stdio.h>
 
-/*
- * The structure is used to bridge the gap between mmap and munmap instructions 
-*/
 struct MMapInfo {
   int sfd; //the file descriptor number
   char * buffer; // the buffer where mmap instruction is mapped
