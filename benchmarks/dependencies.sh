2444c861a1181034c68ff2d31f346e4b6ae37b87
shoaibCS <19100080@lums.edu.pk>
2021-06-24 03:26:06 +0500

    Fixed permission issue

--- benchmarks/dependencies.sh
+++ benchmarks/dependencies.sh
@@ -1,13 +1,13 @@
-sudo apt-get install gettext
-sudo apt-get install autoconf
-sudo apt-get install libbz2-dev
-sudo apt-get install libssl-dev
-sudo apt-get install libpcre3-dev libpcre3
-sudo apt-get install libpcap-dev
-sudo apt-get install dnsutils
-sudo apt-get install groff-base
-sudo apt-get install gnutls-dev
-sudo apt-get install -y pkg-config
-sudo apt-get install libevent-dev
-sudo apt-get install libidn11-dev
-sudo apt-get install uuid-dev
+ apt-get install gettext
+ apt-get install autoconf
+ apt-get install libbz2-dev
+ apt-get install libssl-dev
+ apt-get install libpcre3-dev libpcre3
+ apt-get install libpcap-dev
+ apt-get install dnsutils
+ apt-get install groff-base
+ apt-get install gnutls-dev
+ apt-get install -y pkg-config
+ apt-get install libevent-dev
+ apt-get install libidn11-dev
+ apt-get install uuid-dev
