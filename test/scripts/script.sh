9d72f56a070128d2bbaa30fbbe328746183adf33
aatiranum <aatira.anum@gmail.com>
2021-07-07 15:11:58 +0500

    change script to pass test cases in loop

--- test/scripts/script.sh
+++ test/scripts/script.sh
@@ -4,8 +4,8 @@
 
 cd ${4}
 ${LLVM_CC_NAME} -O0 -Xclang -disable-O0-optnone -emit-llvm -o $3/${1}t1.bc -c ${1}.c
-${LLVM_OPT_NAME} -mem2reg -mergereturn -simplifycfg -loops -lcssa -loop-simplify -loop-rotate $3/${1}t1.bc -o $3/${1}t2.bc
-${LLVM_OPT_NAME} -load ${TRIMMER_HOME}/build/ConstantFolding.so -isAnnotated=false -fileNames $5 -inter-constprop $3/${1}t2.bc -o $3/${1}t3.bc
+${LLVM_OPT_NAME} -mem2reg -loops -lcssa -loop-simplify -loop-rotate -indvars  $3/${1}t1.bc -o $3/${1}t2.bc
+${LLVM_OPT_NAME} -load ${TRIMMER_HOME}/build/ConstantFolding.so -isAnnotated=false -fileNames $5 -inter-constprop $3/${1}t2.bc -o $3/${1}t3.bc 
 ${LLVM_OPT_NAME} -load ${TRIMMER_HOME}/build/Remove.so -remove $3/${1}t3.bc -o $3/${1}t4.bc
 ${LLVM_OPT_NAME} -load ${TRIMMER_HOME}/build/Internalize.so -intern $3/${1}t4.bc -o $3/${1}t5.bc
 ${LLVM_OPT_NAME} -O1 $3/${1}t5.bc -o $3/${2}.bc
