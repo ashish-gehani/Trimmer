3bcf797e4aff03d7864a7addb191ea55ca82eb10
aatiranum <aatira.anum@gmail.com>
2021-11-23 02:32:25 -0800

    add stress tests

---
+++ test/src/stress_tests/test1.c
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2020 SRI International All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+//test to check if Trimmer can handle stress such as large loops with high trip count
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+void branchPruned(int x) {
+    if(x==705082704)
+      printf("branchPruned");
+}
+
+int main(){
+  int count = 0;
+    for(int i = 0; i <= 100000; i++)
+        count += i;
+
+  branchPruned(count);
+  return count;
+}
