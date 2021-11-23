edb7e59b1641b1032ba29db33702474d0f9b27a2
aatiranum <aatira.anum@gmail.com>
2021-11-23 02:30:02 -0800

    create pointer test  case 1

---
+++ test/src/pointer_tests/test1.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2020 SRI International All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+//test that checks the dereferenced value of the pointer 2 (z->a) if it is asigned the value of pointer 1 (t.a)
+
+#include <stdlib.h>
+#include <stdio.h>
+struct temp {
+  int *a;
+};
+
+__attribute__((annotate("specializeArg")))
+void branchPruned(int test) {
+  if (test == 10)
+    printf("branchPruned");
+}
+
+int external();
+void checkptCall();
+
+__attribute__((annotate("specializeArg")))
+int main(int argc, char **argv) {
+  int a = 20;
+  int b = 20; 
+  struct temp t;
+  struct temp *z = (struct temp *)malloc(sizeof(struct temp));
+  t.a = (int*)malloc(sizeof(int)); 
+  z->a = t.a;
+  *t.a = 10;
+    
+  
+  branchPruned(*z->a);
+}
